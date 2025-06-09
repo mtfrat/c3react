81651649c0ce96752853885a1fe87e934ee7ee70
Martin Fraticelli <martinfraticelli@gmail.com>
2025-06-09 19:12:38 -0300

    Fin c3

---
+++ repaso3/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import NavBar from './components/NavBar/NavBar';
+
+const App = () => {
+
+  //Parte de logica
+  return (
+    //Render
+    <>
+    <NavBar/>
+      <h1>Clase 3</h1>
+    </>
+  )
+}
+
+export default App
---
+++ repaso3/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,19 @@
+import "./NavBar.css"
+
+const NavBar = () => {
+  return (
+    <header>
+        <h1>Esperando a Violeta</h1>
+
+        <nav>
+            <ul>
+                <li>Vestidos boda</li>
+                <li>Vestidos 15</li>
+                <li>Casual clothe</li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default NavBar
